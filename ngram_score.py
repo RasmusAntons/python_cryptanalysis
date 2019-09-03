eb5a823a69eaf2ff269299c67fd256ae7a67c7a7
Rasmus Antons <mail@rasmusantons.de>
2019-09-03 17:56:52 +0200

    lazy load and cache ngrams

--- ngram_score.py
+++ ngram_score.py
@@ -6,21 +6,34 @@ from math import log10
 
 
 class ngram_score(object):
+    __cache = {}
+
     def __init__(self, ngramfile, sep=' '):
         """ load a file containing ngrams and counts, calculate log probabilities """
-        self.ngrams = {}
-        for line in open(ngramfile):
-            key, count = line.split(sep)
-            self.ngrams[key] = int(count)
-        self.L = len(key)
-        self.N = sum(self.ngrams.values())
-        # calculate log probabilities
-        for key in self.ngrams.keys():
-            self.ngrams[key] = log10(self.ngrams[key] / self.N)
-        self.floor = log10(0.01 / self.N)
+        self.ngramfile = ngramfile
+        self.sep = sep
+        self.ngrams = None
+
+    def load(self):
+        if self.ngramfile in ngram_score.__cache:
+            self.ngrams = ngram_score.__cache[self.ngramfile]
+        else:
+            self.ngrams = {}
+            for line in open(self.ngramfile):
+                key, count = line.split(self.sep)
+                self.ngrams[key] = int(count)
+            self.L = len(key)
+            self.N = sum(self.ngrams.values())
+            # calculate log probabilities
+            for key in self.ngrams.keys():
+                self.ngrams[key] = log10(self.ngrams[key] / self.N)
+            self.floor = log10(0.01 / self.N)
+            ngram_score.__cache[self.ngramfile] = self.ngrams
 
     def score(self, text):
         """ compute the score of text """
+        if self.ngrams is None:
+            self.load()
         score = 0
         ngrams = self.ngrams.__getitem__
         for i in range(len(text) - self.L + 1):
